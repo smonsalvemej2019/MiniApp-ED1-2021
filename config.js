ace9cffe57d301a19fc80418d84d98ff30db6c1a
Santiago Monsalve <71195627+smonsalvemej2019@users.noreply.github.com>
2021-04-18 21:02:23 -0400

    Database Confiig

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 const config ={
     PORT: 5000 || process.env.PORT,
+    predatabase:{
+        host: 'localhost',
+        user: 'root'
+    },
     database:{
         host: 'localhost',
         user: 'root',
--- db_init.js
+++ db_init.js
@@ -1,19 +1,42 @@
 const mysql = require("mysql");
 const config = require("./config");
+const PreCon = mysql.createConnection(config.predatabase);
 const con = mysql.createConnection(config.database);
 
-con.connect((error)=>{
-    if (error) throw error;
-    console.log('connected');
+PreCon.connect((err)=>{
+    try{
+    if (err) throw err;
+    console.log('Connected');
 
-    con.query(
+    con.query("CREATE DATABASE my_db", (err,result) =>{
+        try{
+        if (err) throw err;
+        console.log('Database created')
+        }catch(err){
+            console.log('Database already exists')
+        }
+    })
+}catch(err){
+    console.log('unable to connect');
+}finally{
+    PreCon.end();
+}
+});
 
-    );
-})
-
-//con.end();
-
-/*create table UserTable (
-    name varchar(255),
-    insertDate datetime default now()
-  );*/
+con.connect((err)=>{
+    try{
+        if (err) throw err;
+        const tableCreate = "CREATE TABLE UserTable (name varchar(255),insertDate datetime default now())"
+        con.query(tableCreate, (err)=>{
+            try{
+                if (err) throw err;
+                console.log('Table created');
+            }catch(err){
+                console.log('Table already exist');
+            }
+        })
+    }catch(err){
+    }finally{
+        con.end();
+    }
+});
